c8b9c31579ab1b9ffb1a16fc9540cac84f756fc5
l3miage-mesouaks <salaheddin.mesouak@etu.univ-grenoble-alpes.fr>
2023-04-28 14:45:51 +0200

    "Héritage entre les tables des utilisateurs"

--- server/src/main/java/fr/uga/l3miage/example/models/Participant.java
+++ server/src/main/java/fr/uga/l3miage/example/models/Participant.java
@@ -3,18 +3,24 @@ package fr.uga.l3miage.example.models;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.Id;
 
 @Entity
 @Getter
 @Setter
-public class Participant extends Utilisateur{
+public abstract class Participant extends Utilisateur{
+
+
+    private Long participantId;
 
     public Participant() {
     }
 
     public Participant(Long userId, String userName, String login) {
         super(userId,userName,login);
+        this.participantId = userId;
     }
 
 }
