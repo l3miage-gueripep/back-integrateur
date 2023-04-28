e100e416a2ca438710555944a9f0801875147fcf
Paul Gueripel <poulbleu.du38@gmail.com>
2023-04-28 16:16:22 +0200

    g enlevé le rouge

--- server/src/main/java/fr/uga/l3miage/example/mapper/QuestionMapper.java
+++ server/src/main/java/fr/uga/l3miage/example/mapper/QuestionMapper.java
@@ -15,19 +15,8 @@ import java.util.stream.Collectors;
 @Mapper(componentModel = "spring")
 public interface QuestionMapper {
     @Mapping(source = "miahoot.id", target = "miahootId")
-    @Mapping(source = "reponses", target = "reponses", qualifiedByName = "mapReponsesToReponseDTOs")
     QuestionDTO toDto(Question entity);
 
     Question toEntity(String label);
-
-    @Named("mapReponsesToReponseDTOs")
-    default List<ReponseDTO> mapReponsesToReponseDTOs(List<Reponse> reponses) {
-        return reponses.stream()
-                .map(reponse -> ReponseDTO.builder()
-                        .id(reponse.getId())
-                        .label(reponse.getLabel())
-                        .build())
-                .collect(Collectors.toList());
-    }
 
 }
