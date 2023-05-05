4a5885eb298fbd4cb15ec5f7f0569dc4f0dc9d7e
l3miage-mesouaks <salaheddin.mesouak@etu.univ-grenoble-alpes.fr>
2023-05-05 12:50:38 +0200

    Implémentation du Repo,DTO,Mapperrr du Session

---
+++ server/src/main/java/fr/uga/l3miage/example/mapper/SessionMapper.java
@@ -0,0 +1,19 @@
+package fr.uga.l3miage.example.mapper;
+
+import fr.uga.l3miage.example.models.Session;
+import fr.uga.l3miage.example.request.CreateSessionRequest;
+import fr.uga.l3miage.example.response.SessionDTO;
+import lombok.NonNull;
+import org.mapstruct.Mapper;
+import org.mapstruct.MappingTarget;
+
+@Mapper(componentModel = "spring")
+public interface SessionMapper {
+
+    SessionDTO toDto(Session entity);
+
+    Session toEntity(CreateSessionRequest request);
+
+    void mergeSession(@MappingTarget @NonNull Session session, SessionDTO sessionDTO);
+
+}
